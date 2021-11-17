6a77e262965e4f4a20f79ca917a03d37f63dc1c5
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2021-11-17 23:05:46 +0100

    Update dart_control.dart

--- bin/dart_control.dart
+++ bin/dart_control.dart
@@ -1,3 +1,19 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+void main() {
+  const doesOneEqualTwo = (1 == 2);
+  print(doesOneEqualTwo);
+
+  const alsoTrue = !(1 == 2);
+  print(alsoTrue);
+
+  const isOneGreaterThanTwo = (1 > 2);
+  const isOneLessThanTwo = (1 < 2);
+  print(isOneLessThanTwo);
+  print(isOneGreaterThanTwo);
+  print(1 <= 2);
+  print(2 <= 1);
+
+  const isSunny = true;
+  const isFinished = true;
+  const willGoCycling = isSunny && isFinished;
+  print(willGoCycling);
 }
