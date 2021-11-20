1ee059a3aeab585a3f7e7921c0780ab2e01946ea
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2021-11-20 21:24:46 +0100

    Update dart_control.dart

--- bin/dart_control.dart
+++ bin/dart_control.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 void main() {
   const doesOneEqualTwo = (1 == 2);
   print(doesOneEqualTwo);
@@ -16,4 +18,45 @@ void main() {
   const isFinished = true;
   const willGoCycling = isSunny && isFinished;
   print(willGoCycling);
+
+  //using OR operator in Dart
+  const willTravelToAustralia = true;
+  const canFindPhoto = false;
+  const canDrawPlatypus = willTravelToAustralia || canFindPhoto;
+  print(canDrawPlatypus);
+
+  //Operator precedence
+
+  const andTrue = 1 < 2 && 4 > 3;
+  const andFalse = 1 < 2 && 3 > 4;
+  const orTrue = 1 < 2 || 3 > 4;
+  const orFalse = 1 == 2 || 3 == 4;
+  print(andTrue);
+  print(andFalse);
+  print(orTrue);
+  print(orFalse);
+
+  //NB!: Use parentheses to interrupt precedence
+
+  //String Equality
+  const guess = 'dog';
+  const dogEqualsCat = guess == 'cat';
+  print(dogEqualsCat);
+
+  ///MINI EXERCISES
+  ///
+  ///Exercise 1
+  const myAge = 21;
+  const teenagers = myAge >= 13 && myAge <= 19;
+  print(teenagers);
+
+  ///Exercise 2
+  ///
+
+  const maryAge = 30;
+  const bothTeenagers =  (myAge >= 13 && myAge <= 19) && (maryAge >= 13 && maryAge <= 19);
+
+  print(bothTeenagers);
+    
+  
 }
