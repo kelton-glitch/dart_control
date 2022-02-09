83e1a7434c15357cb7f1fe10046b354b46e63e90
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-09 22:49:56 +0100

    adding /enum proprty

--- bin/switch.dart
+++ bin/switch.dart
@@ -17,4 +17,23 @@ void main() {
     default:
       print("I'm not familiar with this weather");
   }
-}
+
+  const audioState = AudioState.playing;
+switch (audioState) {
+  case AudioState.playing:
+ print('Audio is playing');
+   break;
+ case AudioState.paused:
+ print('Audio is paused');
+   break;
+ case AudioState.stopped:
+ print('Audio is stopped');
+   break;
+ }
+} 
+
+enum AudioState {
+  playing,
+  paused,
+  stopped
+} 
