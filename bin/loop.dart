76ab6003b9dcbd9722ad5de82792c1f00a8d0e88
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-10 13:02:29 +0100

    for loop

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,15 +1,23 @@
 import 'dart:math';
 
 void main() {
+  /// WHILE loop
   var sum = 1;
   while (sum < 10) {
     sum += 4;
     print(sum);
   }
 
+  /// random
   final random = Random();
   while (random.nextInt(7) + 1 != 7) {
     print('Not a seven!');
   }
   print('Finally, you got a seven!');
+
+  ///FOR loop
+ var i=0;
+  for (i = 0; i < 10; i++) {
+    print(i);
+  }
 }
