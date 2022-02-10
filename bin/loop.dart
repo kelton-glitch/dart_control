1b3608ce1a2cb8ded2dfafff8a93fc6082f4ce41
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-10 12:59:25 +0100

    rand with while

--- bin/loop.dart
+++ bin/loop.dart
@@ -1,7 +1,15 @@
+import 'dart:math';
+
 void main() {
   var sum = 1;
   while (sum < 10) {
     sum += 4;
     print(sum);
   }
+
+  final random = Random();
+  while (random.nextInt(7) + 1 != 7) {
+    print('Not a seven!');
+  }
+  print('Finally, you got a seven!');
 }
