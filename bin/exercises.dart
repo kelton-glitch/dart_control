96c2b9e7d791717bbe400698cac55c27012ba893
Kamgo N Elton <76106656+kelton-glitch@users.noreply.github.com>
2022-02-11 23:52:32 +0100

    exercise2

--- bin/exercises.dart
+++ bin/exercises.dart
@@ -1,8 +1,15 @@
 void main() {
   var counter = 0;
 
-  while(counter <10 ){
+  while (counter < 10) {
     print('counter is ');
-    counter ++;
+    counter++;
+  }
+
+  var int = 0;
+
+  for (int = 0; int <= 10; int++) {
+    print(int);
+    print(int * int);
   }
 }
